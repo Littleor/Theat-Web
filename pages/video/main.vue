1304ab56906f8f2e9285b3d11c7400321883f723
Little <admin@sixming.com>
2020-03-28 10:13:59 +0800

    重构结构

--- pages/video/main.vue
+++ pages/video/main.vue
@@ -1,8 +1,8 @@
 <template>
 	<view class="contain">
 		<view class="video">
-			<video  id="video" :src="client.src" @error="onVideoError" @play="onVideoPlay" @pause="onVideoPause"
-			 @waiting="onVideoWait" @timeupdate="onPlayUpdate" controls vslide-gesture></video>
+			<video id="video" :src="client.src" @error="onVideoError" @play="onVideoPlay" @pause="onVideoPause" @waiting="onVideoWait"
+			 @timeupdate="onPlayUpdate" controls vslide-gesture></video>
 		</view>
 		<view class="action">
 			<uni-card class="card" title="操作" :extra="'当前人数: ' + this.roomNumber + ' 人'">
@@ -53,6 +53,8 @@
 				roomNumber: 0,
 				waitOthers: false,
 				getMessage: false,
+				waitForResponse: '',
+				execute: ''
 			}
 		},
 		onLoad(res) {
@@ -124,7 +126,7 @@
 			reload() {
 
 			},
-			onPlayUpdate:function(e){
+			onPlayUpdate: function(e) {
 				//正在播放的时候 250ms调用
 				let duration = e.detail.duration;
 				let currentTime = e.detail.currentTime;
@@ -132,75 +134,278 @@
 					duration = e.detail.detail.duration;
 					currentTime = e.detail.detail.currentTime;
 				}
+				if(Math.abs(currentTime - this.client.status.process) >= 0.6){
+					//调整了进度
+					this.client.status.process = currentTime;
+					this.client.status.duration = duration;
+					this.onProcessChange();
+					return;
+				}
 				this.client.status.process = currentTime;
 				this.client.status.duration = duration;
 			},
 			update() {
-				
+
 			},
 			onVideoWait: function(e) {
-				console.log("加载中");
+				if(this.waitForResponse == '' && this.execute == ''){
+					//非人工因数导致的加载
+					console.log("非人工因数导致的加载");
+					this.onChangePlayStatus('load');
+				}
+				else{
+					console.log("人工因数导致加载ing");
+				}
 				this.client.status.loading = true;
 				let currentTime = this.client.status.process;
-				this.onChangePlayStatus();
 				let interval = setInterval(() => {
-					if(Math.abs(this.client.status.process - currentTime) >= 0.30){
+					if (Math.abs(this.client.status.process - currentTime) >= 0.30) {
 						//加载完成
 						this.onVideoLoaded();
 						clearInterval(interval);
 					}
 				}, 250);
+				
 			},
-			onVideoLoaded(){
-					//当视频加载完成
-					console.log("加载完成");
-					this.client.status.loading = false;
-					this.onChangePlayStatus();
+			onVideoLoaded() {
+				//当视频加载完成
+				console.log("加载完成");
+				this.client.status.loading = false;
+				// if (this.waitForResponse == 'play') {
+				// 	//自己Play加载完成后暂停
+				// 	console.log('加载完成Pause等待对方加载');
+				// 	this.videoContext.pause();
+				// }
+				if (this.execute == 'play') {
+					//响应对方请求Play
+					console.log('加载完成对方的请求Play');
+					this.reponse('play');
+				}
+				else if (this.execute == 'process')
+				{
+					//响应对方的process
+					console.log('加载完成对方的请求process');
+					this.reponse('process');
+					
+				}
+				else if(this.waitForResponse == 'process'){
+				}
+				else if(this.execute == 'loaded'){
+					//响应对方的loaded
+					console.log('加载完成对方的请求loaded');
+					this.reponse('loaded');
+				}
+				else if(this.execute == '' && this.waitForResponse == ''){
+					this.onChangePlayStatus('loaded');
+					this.videoContext.pause();
+				}
+				else{}
 			},
 			onVideoPlay: function(e) {
 				this.client.status.playing = true;
-				if(this.getMessage){
+				if (this.execute == 'play') {
 					console.log("对方启动播放");
+				} else if (this.waitForResponse == 'play') {
+					console.log("对方response播放请求");
+				}
+				else if(this.waitForResponse == 'process'){
+					console.log("对方response修改进度请求");
+				}
+				else if(this.waitForResponse == 'loaded'){
+					console.log("对方response等待请求");
+				}
+				 else if(this.waitForResponse == '' && this.execute == ''){
+					console.log("自己启动播放");
+					this.onChangePlayStatus('play');
 				}
 				else{
-					console.log("自己启动播放");
-					this.onChangePlayStatus();
+					
 				}
 			},
 			onVideoPause: function(e) {
 				this.client.status.playing = false;
-				if(this.waitOthers){
-					console.log('暂停等待对方加载完成');
+				if (this.waitForResponse == 'play') {
+					//代表等待对方播放的时候Pause
+					console.log('代表等待对方播放的时候Pause');
+				} else if (this.execute == 'pause') {
+					//响应对方请求暂停
+					console.log('响应对方请求暂停');
+					this.reponse('pause');
+				} else if (this.waitForResponse == 'pause') {
+					console.log('对方response暂停');
+				} else if(this.execute == 'load')
+				{
+					console.log('响应对方请求load');
+					this.reponse('load');
 				}
-				else if(!this.waitOthers && this.getMessage){
-					console.log("对方暂停了");
+				else if(this.waitForResponse == 'loaded'){
+					console.log('代表等待对方loaded的时候');
 				}
-				else if(!this.waitOthers && !this.getMessage){
-					console.log("自己暂停了");
-					this.onChangePlayStatus();
+				else if(this.waitForResponse == '' && this.execute == ''){
+					//自己发起请求
+					console.log('自己发起Pause请求');
+					this.onChangePlayStatus('pause');
 				}
-				else{
-					console.log("莫名其妙暂停了");
-				}
+				else{}
+
 			},
-			onProcessChange(){
+			onProcessChange() {
 				//当用户自己手动调整进度
-				console.log("自己手动调整进度条");
-				this.onChangePlayStatus();
+				console.log("自己手动调整进度条并已加载完成");
+				this.onChangePlayStatus('process');
+				this.videoContext.pause();
 			},
-			changePlayStatus(){
-				
+			changePlayStatus() {
+
 			},
 			onVideoError: function(e) {
 				console.log(e);
 			},
-			onChangePlayStatus: function() {
-				if (this.client.room != "" && this.roomNumber != 0 && !this.getMessage) {
-					console.log(this.getClient());
-					this.socket.emit("adjust", this.getClient());
-				}
+			reponse:function(type) {
+				this.execute = '';
+				this.waitForResponse = '';
+				this.client.status.type = type;
+				this.socket.emit(type, this.getClient());
+				this.client.status.type = '';
+			},
+			onChangePlayStatus: function(type) {
+				this.client.status.type = type;
+				this.waitForResponse = type;
+				this.socket.emit(type, this.getClient());
+				this.client.status.type = '';
 			},
 			addListener: function() {
+				this.socket.on('loaded', (data) => {
+					if (this.waitForResponse == 'loaded') {
+						//用户发出之后接到对方的response
+						console.log('对方响应了自己的load');
+						this.videoContext.play();
+						this.waitForResponse = '';
+					} else {
+						//用户接到对方的请求
+						this.execute = 'loaded';
+						this.videoContext.play();
+					}
+				})
+				this.socket.on('load', (data) => {
+					if (this.waitForResponse == 'load') {
+						//用户发出之后接到对方的response
+						console.log('对方响应了自己的load');
+						this.waitForResponse = '';
+					} else {
+						//用户接到对方的请求
+						this.execute = 'load';
+						this.videoContext.pause();
+					}
+				})
+				this.socket.on('process', (data) => {
+					let {
+						id,
+						room,
+						status,
+						sendTime,
+						time
+					} = data;
+					let {
+						playing,
+						loading,
+						process,
+						control,
+						duration,
+						type
+					} = status;
+					let delay = new Date().getTime() - sendTime;
+					process = process + delay / 1000.0;
+					if (this.waitForResponse == 'process') {
+						//用户发出之后接到对方的response
+						console.log('对方响应了自己的process');
+						this.videoContext.play();
+						this.waitForResponse = '';
+					} else {
+						//用户接到对方播放的请求
+						this.execute = 'process';
+						this.videoContext.seek(process);
+					}
+				})
+
+				this.socket.on('pause', (data) => {
+					if (this.waitForResponse == 'pause') {
+						//用户发出之后接到对方的response
+						console.log('对方响应了自己的pause');
+						this.waitForResponse = '';
+					} else {
+						//用户接到对方的请求
+						this.execute = 'pause';
+						this.videoContext.pause();
+					}
+				})
+
+				this.socket.on('play', (data) => {
+					let {
+						id,
+						room,
+						status,
+						sendTime,
+						time
+					} = data;
+					let {
+						playing,
+						loading,
+						process,
+						control,
+						duration,
+						type
+					} = status;
+					if (this.waitForResponse == type) {
+						//用户发出之后接到对方的response
+						this.videoContext.play();
+						this.waitForResponse = '';
+					} else {
+						//用户接到对方播放的请求
+						this.execute = type;
+						this.videoContext.seek(process);
+						this.videoContext.play();
+					}
+				})
+
+				//收到事件
+				this.socket.on('adjust', (data) => {
+					this.getMessage = true;
+					let {
+						id,
+						room,
+						status,
+						sendTime,
+						time
+					} = data;
+					let {
+						playing,
+						loading,
+						process,
+						control,
+						duration,
+						type
+					} = status;
+					let delay = new Date().getTime() - sendTime;
+					process = process + delay / 1000.0;
+
+					this.client.status = status;
+					this.videoContext.seek(process);
+					if (loading) {
+						this.waitOthers = true;
+					} else {
+						this.waitOthers = false;
+					}
+					if (playing && !loading && !this.client.status.loading) {
+						this.videoContext.play();
+					} else {
+						this.videoContext.pause();
+					}
+					setTimeout(() => {
+						this.getMessage = false;
+					}, 320);
+				});
+
 				// 连接成功
 				this.socket.on('connect', () => {
 					console.log('连接成功');
@@ -260,43 +465,6 @@
 					this.client.src = src;
 					this.roomNumber = number;
 					console.log(data);
-				})
-				//收到事件处理
-				this.socket.on('adjust', (data) => {
-					this.getMessage = true;
-					let {
-						id,
-						room,
-						status,
-						sendTime,
-						time
-					} = data;
-					let {
-						playing,
-						loading,
-						process,
-						control,
-						duration
-					} = status;
-					let delay = new Date().getTime() - sendTime;
-					process = process + delay / 1000.0;
-					this.client.status = status;
-					this.videoContext.seek(process);
-					
-					if (loading) {
-						this.waitOthers = true;
-					} else {
-						this.waitOthers = false;
-					}
-					if(playing && !loading && !this.client.status.loading){
-						this.videoContext.play();
-					}
-					else {
-						this.videoContext.pause();
-					}
-					setTimeout(() => {
-						this.getMessage = false;
-					}, 320);
 				})
 			},
 			getClient: function() {
